1014f33dfa9646c27172d2c7be2160f7f295b150
Ramin Mammadzada <remki-design@hotmail.com>
2021-08-15 19:24:06 +0400

    Remove unused code lines.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,6 @@ import { environment } from '../environments/environment';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  // public isCreate: boolean;
   public name: string;
   public fileSize: number;
   public fileName: string;
@@ -31,7 +30,6 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(){
-    // this.isCreate = true;
     this.getUsers();
   }
 
@@ -47,17 +45,14 @@ export class AppComponent implements OnInit {
 
     console.log("this.file: ", this.file);
 
-    // this.http.post('https://localhost:5001/api/users', this.user)
     this.http.post(environment.apiURL + 'api/files', this.file)
     .subscribe(res => {
       this.getUsers();
-      // this.isCreate = false;
 
     });
   }
 
   private getUsers = () => {
-    // this.http.get('https://localhost:5001/api/users')
     this.http.get(environment.apiURL + 'api/files')
     .subscribe(res => {
       this.files = res as File[];
@@ -86,7 +81,6 @@ export class AppComponent implements OnInit {
   }
 
   public createImgPath = (serverPath: string) => {
-    // return `https://localhost:5001/${serverPath}`;
     return `${environment.apiURL}${serverPath}`;
   }
 }
--- src/app/staticData.ts
+++ src/app/staticData.ts
@@ -1,2 +1,2 @@
-export const FILE_SIZE_LIMIT = 150000; // in KB
+export const FILE_SIZE_LIMIT = 150000; // in bytes
 export const ALLOWED_FILE_TYPES = ['jpeg', 'html', 'png', 'json'];
